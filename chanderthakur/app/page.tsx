e0b403dbff066314fd6512d22e201c7a9618b663
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2025-02-24 23:13:39 +0530

    First commit

---
+++ chanderthakur/app/page.tsx
@@ -0,0 +1,13 @@
+
+import { CarouselDemo } from "@/components/ui/carousel";
+import Example from "@/components/ui/hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+    <Example />
+    <CarouselDemo />
+</div>
+  );
+}
