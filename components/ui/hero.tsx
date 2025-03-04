b6ce82bffad056cb90eca33b33054f56586afde9
MOON-CARVER <126223451+MOON-CARVER@users.noreply.github.com>
2025-03-04 19:36:33 +0530

    hero updated

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -75,14 +75,7 @@ export default function Example() {
                     </a>
                   ))}
                 </div>
-                <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                  >
-                    Log in
-                  </a>
-                </div>
+                
               </div>
             </div>
           </DialogPanel>
@@ -105,16 +98,16 @@ export default function Example() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
           <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+            <h1 className="text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+              Hi,<br/>
+              My name is Chander 
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+              I am Software Developer/ Editor, welcome to my portfolio
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="#"
+                href="https://drive.google.com/file/d/1vrknjSNwqhkZ2FbNipjOZN3TDqlA_dwj/view?usp=sharing"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Resume
